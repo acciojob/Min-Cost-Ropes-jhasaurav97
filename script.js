dd66db2bead4dd933aa8eb17f9c1705252e31ab9
Saurav <jhasaurav593@gmail.com>
2025-06-07 17:26:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 function mincost(arr)
 { 
 //write your code here
+	arr.sort((a, b) => a - b);
+	let totalCost = 0;
+	while(arr.length > 1){
+		let first = arr.shit();
+		let second = arr.shit();
+		let cost = first + second;
+		totalCost += cost;
+
+		let inserted = false;
+		for(let i = 0; i < arr.length; i++){
+			if(cost < arr[i]){
+				arr.splice(i, 0, cost);
+				inserted = true;
+				break;
+			}
+		}
+			if(!inserted) arr.push(cost);
+	}
 // return the min cost
+		return totalCost;
   
 }
+
+
 
 module.exports=mincost;
